adc7687bb91f39db64c96c865de9a15f2d678848
abelt88 <adriennebelt@gmail.com>
2024-11-14 04:07:45 -0500

    Final project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,21 @@ module.exports = {
     './*.{html,js,md}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        clay: '#745751',
+        cream: '#F5EBE2',
+        olive: '#A29572',
+        herb: '#BAB7A6',
+        sand: '#B59C7D',
+        forest: '#696F55',
+      },
+      fontFamily: {
+        body: ["Montserrat", "sans-serif"],
+        heading: ["Italiana", "serif"],
+        
+      }
+    },
   },
   plugins: [],
 }
